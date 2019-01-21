b803aa9e3c6358e84af9c29126cccd45d6774485
yruamau <amaury.fouville@gmail.com>
2019-01-21 13:06:49 +0100

    Update js.js

--- static/js/js.js
+++ static/js/js.js
@@ -1,5 +1,5 @@
-$(".jumbotron").css({ height: $(window).height() + "px" });
+$(".jumbotron").css({ height: $(window).width() + "px" });
 
 $(window).on("resize", function() {
-  $(".jumbotron").css({ height: $(window).height() + "px" });
+  $(".jumbotron").css({ height: $(window).width() + "*3768/7478px" });
 });
