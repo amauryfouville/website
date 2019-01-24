9ec8196200a510222f70174319ad3a93c7c5a7f0
Amaury Fouville <amaury.fouville2@gmail.com>
2019-01-24 10:54:52 +0100

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 
 #Create instance of Flask App
-app = Flask(__name__)
+app = Flask(amaury)
 
 #Define Route and content of that page
 @app.route("/")
@@ -11,7 +11,7 @@ def home():
 
 #Define 2nd route and content
 @app.route("/formation")
-def about():
+def formation():
     return render_template("formation.html")
 
 @app.route("/atouts")
@@ -19,5 +19,5 @@ def atouts():
     return render_template("atouts.html")
 
 #Running and Controlling the script
-if (__name__ =="__main__"):
+if (amaury =="amaury"):
 	app.run(port=80, host=0.0.0.0, debug=True)
