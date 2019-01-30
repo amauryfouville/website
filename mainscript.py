137cada98a5e678bf5e1d3f00ca157396ea0cfe2
Amaury Fouville <amaury.fouville2@gmail.com>
2019-01-30 21:40:44 +0100

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -1,15 +1,32 @@
-#Import dependencies
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from models import locations
 
 #Create instance of Flask App
 app = Flask(__name__)
 
-#Define Route and content of that page
+db = SQLAlchemy()
+
+POSTGRES = {
+    'user': 'vwbdbcvjiqxqsd',
+    'pw': 'eb5fbd3b9b1367b125c223d7956aa1414038e565bc01f317606b1e30a5c4d38f',
+    'db': 'dc7phhigdmm0d8',
+    'host': 'ec2-54-228-224-37.eu-west-1.compute.amazonaws.com',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+db.init_app(app)
+
+#Définitioin des routes et des contenus des pages
+
+#Accueil / Compétences, à séparer
 @app.route("/")
 def home():
     return render_template("index.html")
 
-#Define 2nd route and content
+#formation
 @app.route("/formation")
 def formation():
     return render_template("formation.html")
@@ -20,7 +37,27 @@ def atouts():
 
 @app.route("/map")
 def map():
+	if request.method == 'POST':
+	    name=request.args.get('name')
+	    lat=request.args.get('lat')
+	    long=request.args.get('long')
+	    type=request.args.get('type')
+	    try:
+	        location=location(
+	            name=name,
+	            lat=lat,
+	            long=long,
+	            type=type
+	        )
+	        db.session.add(location)
+	        db.session.commit()
+	        return "Locationk added. location id={}".format(location.id)
+	    except Exception as e:
+			return(str(e))
     return render_template("maps.html")
+
+@app.route("/add")
+
 
 #Running and Controlling the script
 if (__name__ =="__main__"):
