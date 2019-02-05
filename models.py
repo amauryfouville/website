c645dd0d060b71bafb471733f38c29e33ae6bc3e
Amaury Fouville <amaury.fouville2@gmail.com>
2019-02-05 15:41:49 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,29 +1,33 @@
-from mainscript import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class location(db.Model):
     __tablename__ = 'locations'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    lat = db.Column(db.Float())
-    long = db.Column(db.FLoat())
-    type = db.Column(db.String())
+    name = db.Column(db.String(60),nullable=False)
+    lat = db.Column(db.Float(), nullable=False)
+    long = db.Column(db.Float(), nullable =False)
+    type = db.Column(db.String(15), nullable=False)
     
 
-    def __init__(self, name, author, published):
+    def __init__(self, name, lat, long, type):
         self.name = name
         self.lat = lat
         self.long = long
         self.type = type
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return '<Name %r>' % self.name
     
     def serialize(self):
         return {
             'id': self.id, 
             'name': self.name,
             'lat': self.lat,
-            'long':self.long,
-            'type':self.type
-            }
+            'long': self.long,
+            'type': self.type
+            } 
+
